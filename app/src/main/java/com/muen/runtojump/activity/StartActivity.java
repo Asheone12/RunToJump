a640023eb84d0abed6cfad9ff17fb96f2c2cc463
xujianglong <jianglong.xu@tiho-mobi.com>
2024-03-28 16:31:58 +0800

    首次提交，实现基本功能

---
+++ app/src/main/java/com/muen/runtojump/activity/StartActivity.java
@@ -0,0 +1,101 @@
+package com.muen.runtojump.activity;
+
+import android.app.Activity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import com.muen.runtojump.App;
+import com.muen.runtojump.R;
+
+import java.util.Calendar;
+
+
+/**
+ * 游戏的开始菜单页，用户可选择难度和退出游戏操作
+ */
+public class StartActivity extends Activity {
+    private TextView one = null;
+    private TextView two = null;
+    private TextView three = null;
+    private TextView copyRight = null;
+    private ImageView logo = null;
+    private AnimationDrawable ad = null;
+    private MyClickEvent myClickEvent = null;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start);
+        this.one = (TextView) super.findViewById(R.id.one);
+        this.two = (TextView) super.findViewById(R.id.two);
+        this.three = (TextView) super.findViewById(R.id.three);
+        this.copyRight = (TextView) super.findViewById(R.id.copyRight);
+        this.logo = (ImageView) super.findViewById(R.id.logo);
+        this.ad = (AnimationDrawable) this.logo.getBackground();
+        this.myClickEvent = new MyClickEvent();
+        this.one.setOnClickListener(this.myClickEvent);
+        this.two.setOnClickListener(this.myClickEvent);
+        this.three.setOnClickListener(this.myClickEvent);
+
+        this.copyRight.setText(super.getString(R.string.copyRight, String.valueOf(Calendar.getInstance().get(Calendar.YEAR))));
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            this.ad.start();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this).setTitle(R.string.app_name)
+                .setMessage("确定要退出游戏？")
+                .setPositiveButton("退出",
+                        new DialogInterface.OnClickListener() {
+
+                            public void onClick(DialogInterface dialog,
+                                                int which) {
+                                finish();
+                            }
+                        }).setNegativeButton("取消", null).show();
+    }
+
+    private class MyClickEvent implements OnClickListener {
+
+        public void onClick(View v) {
+
+            int gameType = -1;
+            switch (v.getId()) {
+                case R.id.one:
+                    gameType = App.GAMETYPE_ONE;
+                    break;
+                case R.id.two:
+                    gameType = App.GAMETYPE_TWO;
+                    break;
+                case R.id.three:
+                    gameType = App.GAMETYPE_THREE;
+                    break;
+
+            }
+            if (gameType != -1) {
+                Intent intent = new Intent(StartActivity.this,
+                        RunningGameActivity.class);
+                intent.putExtra(App.GAMETYPE, gameType);
+                startActivity(intent);
+                finish();
+            }
+
+        }
+    }
+}
